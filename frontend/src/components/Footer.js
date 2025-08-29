35848b3dc73859ba0b56de77caa897bc1429b884
Andre Urban <andreurbanwork@gmail.com>
2025-08-29 17:40:49 -0300

    atualizaçao footer e layout

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -7,20 +7,35 @@ export default function Footer() {
     <Box
       component="footer"
       sx={{
+        position: 'fixed',
+        bottom: 0,
+        left: 0,
+        width: '100%',
         bgcolor: 'grey.900',
         color: 'grey.100',
         py: 2,
         textAlign: 'center',
-        mt: 'auto',
+        boxShadow: '0 -2px 6px rgba(0,0,0,0.2)',
+        zIndex: 1201,
       }}
     >
       <Typography variant="body2" sx={{ fontSize: '0.85rem' }}>
         © {new Date().getFullYear()} - Seu Projeto |{' '}
-        <Link href="https://github.com/AndUrban/task-manager-fullstack" underline="hover" color="inherit" sx={{ fontWeight: 'bold' }}>
+        <Link
+          href="https://github.com/AndUrban/task-manager-fullstack"
+          underline="hover"
+          color="inherit"
+          sx={{ fontWeight: 'bold' }}
+        >
           Github
         </Link>{' '}
         |{' '}
-        <Link href="#" underline="hover" color="inherit" sx={{ fontWeight: 'bold' }}>
+        <Link
+          href="#"
+          underline="hover"
+          color="inherit"
+          sx={{ fontWeight: 'bold' }}
+        >
           Link 2
         </Link>
       </Typography>
