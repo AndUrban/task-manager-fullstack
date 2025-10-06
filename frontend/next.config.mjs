564437aca294f913138ff4cc88c5cd4627c45e18
Andre Urban <andreurbanwork@gmail.com>
2025-10-06 18:07:26 -0300

    frontend/dockerfile; next.config.mjs; postcss.config.js

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   compiler: {
-    styledComponents: true
-  }
+    styledComponents: true // mantém suporte ao styled-components
+  },
+
+  reactStrictMode: true,
+  swcMinify: true,
+
+  // ativa suporte a pipeline CSS Modules e suporte a importação de CSS global
+  experimental: {
+    css: true,
+    esmExternals: true, // evita conflito com postcss/autoprefixer
+  },
 };
 
 export default nextConfig;
